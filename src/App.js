17086d50b8b1226793d0fc3b431f9b5fda799742
unknown <anna.f@she-codes.org>
2022-09-03 16:51:42 +0300

    useRefHook example component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import UseReducerHook from './components/useReducerHook.js';
+import UseRefHook from './components/useRefHook';
+
 function App() {
   return (
     <div className="App">   
       <UseReducerHook />
+      <UseRefHook />
 
     </div>
   );
---
+++ src/components/useRefHook.js
@@ -0,0 +1,21 @@
+import React, { useRef } from 'react';
+
+export default function UseRefHook(){
+
+    const inputRef = useRef(null);
+    const inputRef2 = useRef(null);
+
+    const logInput = () => console.log(inputRef.current.value);
+    const focuseOn = () => inputRef2.current.focus();
+
+    return (
+        <>
+            <h1>useRefHook</h1>
+            <input type="text" placeholder="Ex..." ref={inputRef} />
+            <button onClick={logInput}>Change me</button> 
+
+            <input type="text" placeholder="Ex..." ref={inputRef2} />
+            <button onClick={focuseOn}>Focuse on me</button>
+        </>
+    );
+}
